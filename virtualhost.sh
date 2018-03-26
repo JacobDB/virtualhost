cfc45e4a512190a4a2961264cf945d37d3d98679
Jacob Bearce <jacob@bearce.me>
2018-03-26 16:28:08 -0500

    Tabs => Spaces

--- virtualhost.sh
+++ virtualhost.sh
@@ -16,162 +16,162 @@ sitesAvailabledomain=$sitesAvailable$domain.conf
 ### don't modify from here unless you know what you are doing ####
 
 if [ "$(whoami)" != 'root' ]; then
-	echo $"You have no permission to run $0 as non-root user. Use sudo"
-		exit 1;
+    echo $"You have no permission to run $0 as non-root user. Use sudo"
+        exit 1;
 fi
 
 if [ "$action" != 'create' ] && [ "$action" != 'delete' ]
-	then
-		echo $"You need to prompt for action (create or delete) -- Lower-case only"
-		exit 1;
+    then
+        echo $"You need to prompt for action (create or delete) -- Lower-case only"
+        exit 1;
 fi
 
 while [ "$domain" == "" ]
 do
-	echo -e $"Please provide domain. e.g.dev,staging"
-	read domain
+    echo -e $"Please provide domain. e.g.dev,staging"
+    read domain
 done
 
 if [ "$rootDir" == "" ]; then
-	rootDir=${domain//./}
+    rootDir=${domain//./}
 fi
 
 ### if root dir starts with '/', don't use /var/www as default starting point
 if [[ "$rootDir" =~ ^/ ]]; then
-	userDir=''
+    userDir=''
 fi
 
 rootDir=$userDir$rootDir
 
 if [ "$action" == 'create' ]
-	then
-		### check if domain already exists
-		if [ -e $sitesAvailabledomain ]; then
-			echo -e $"This domain already exists.\nPlease Try Another one"
-			exit;
-		fi
+    then
+        ### check if domain already exists
+        if [ -e $sitesAvailabledomain ]; then
+            echo -e $"This domain already exists.\nPlease Try Another one"
+            exit;
+        fi
 
-		### check if directory exists or not
-		if ! [ -d $rootDir ]; then
-			### create the directory
-			mkdir $rootDir
-			### give permission to root dir
-			chmod 755 $rootDir
-			### write test file in the new domain dir
-			if ! echo "<?php echo phpinfo(); ?>" > $rootDir/phpinfo.php
-			then
-				echo $"ERROR: Not able to write in file $rootDir/phpinfo.php. Please check permissions"
-				exit;
-			else
-				echo $"Added content to $rootDir/phpinfo.php"
-			fi
-		fi
+        ### check if directory exists or not
+        if ! [ -d $rootDir ]; then
+            ### create the directory
+            mkdir $rootDir
+            ### give permission to root dir
+            chmod 755 $rootDir
+            ### write test file in the new domain dir
+            if ! echo "<?php echo phpinfo(); ?>" > $rootDir/phpinfo.php
+            then
+                echo $"ERROR: Not able to write in file $rootDir/phpinfo.php. Please check permissions"
+                exit;
+            else
+                echo $"Added content to $rootDir/phpinfo.php"
+            fi
+        fi
 
-		### create virtual host rules file
-		if ! echo "
-		<VirtualHost *:80>
-			ServerAdmin $email
-			ServerName $domain
-			ServerAlias $domain
-			DocumentRoot $rootDir
-			<Directory />
-				AllowOverride All
-			</Directory>
-			<Directory $rootDir>
-				Options Indexes FollowSymLinks MultiViews
-				AllowOverride all
-				Require all granted
-			</Directory>
-			ErrorLog /var/log/apache2/$domain-error.log
-			LogLevel error
-			CustomLog /var/log/apache2/$domain-access.log combined
-		</VirtualHost>" > $sitesAvailabledomain
-		then
-			echo -e $"There is an ERROR creating $domain file"
-			exit;
-		else
-			echo -e $"\nNew Virtual Host Created\n"
-		fi
+        ### create virtual host rules file
+        if ! echo "
+        <VirtualHost *:80>
+            ServerAdmin $email
+            ServerName $domain
+            ServerAlias $domain
+            DocumentRoot $rootDir
+            <Directory />
+                AllowOverride All
+            </Directory>
+            <Directory $rootDir>
+                Options Indexes FollowSymLinks MultiViews
+                AllowOverride all
+                Require all granted
+            </Directory>
+            ErrorLog /var/log/apache2/$domain-error.log
+            LogLevel error
+            CustomLog /var/log/apache2/$domain-access.log combined
+        </VirtualHost>" > $sitesAvailabledomain
+        then
+            echo -e $"There is an ERROR creating $domain file"
+            exit;
+        else
+            echo -e $"\nNew Virtual Host Created\n"
+        fi
 
-		### Add domain in /etc/hosts
-		if ! echo "127.0.0.1	$domain" >> /etc/hosts
-		then
-			echo $"ERROR: Not able to write in /etc/hosts"
-			exit;
-		else
-			echo -e $"Host added to /etc/hosts file \n"
-		fi
+        ### Add domain in /etc/hosts
+        if ! echo "127.0.0.1    $domain" >> /etc/hosts
+        then
+            echo $"ERROR: Not able to write in /etc/hosts"
+            exit;
+        else
+            echo -e $"Host added to /etc/hosts file \n"
+        fi
 
-		### Add domain in /mnt/c/Windows/System32/drivers/etc/hosts (Windows Subsytem for Linux)
-		if [ -e /mnt/c/Windows/System32/drivers/etc/hosts ]
-		then
-			if ! echo -e "\r127.0.0.1       $domain" >> /mnt/c/Windows/System32/drivers/etc/hosts
-			then
-				echo $"ERROR: Not able to write in /mnt/c/Windows/System32/drivers/etc/hosts (Hint: Try running Bash as administrator)"
-			else
-				echo -e $"Host added to /mnt/c/Windows/System32/drivers/etc/hosts file \n"
-			fi
-		fi
+        ### Add domain in /mnt/c/Windows/System32/drivers/etc/hosts (Windows Subsytem for Linux)
+        if [ -e /mnt/c/Windows/System32/drivers/etc/hosts ]
+        then
+            if ! echo -e "\r127.0.0.1       $domain" >> /mnt/c/Windows/System32/drivers/etc/hosts
+            then
+                echo $"ERROR: Not able to write in /mnt/c/Windows/System32/drivers/etc/hosts (Hint: Try running Bash as administrator)"
+            else
+                echo -e $"Host added to /mnt/c/Windows/System32/drivers/etc/hosts file \n"
+            fi
+        fi
 
-		if [ "$owner" == "" ]; then
-			chown -R $(whoami):$(whoami) $rootDir
-		else
-			chown -R $owner:$owner $rootDir
-		fi
+        if [ "$owner" == "" ]; then
+            chown -R $(whoami):$(whoami) $rootDir
+        else
+            chown -R $owner:$owner $rootDir
+        fi
 
-		### enable website
-		a2ensite $domain
+        ### enable website
+        a2ensite $domain
 
-		### restart Apache
-		/etc/init.d/apache2 reload
+        ### restart Apache
+        /etc/init.d/apache2 reload
 
-		### show the finished message
-		echo -e $"Complete! \nYou now have a new Virtual Host \nYour new host is: http://$domain \nAnd its located at $rootDir"
-		exit;
-	else
-		### check whether domain already exists
-		if ! [ -e $sitesAvailabledomain ]; then
-			echo -e $"This domain does not exist.\nPlease try another one"
-			exit;
-		else
-			### Delete domain in /etc/hosts
-			newhost=${domain//./\\.}
-			sed -i "/$newhost/d" /etc/hosts
+        ### show the finished message
+        echo -e $"Complete! \nYou now have a new Virtual Host \nYour new host is: http://$domain \nAnd its located at $rootDir"
+        exit;
+    else
+        ### check whether domain already exists
+        if ! [ -e $sitesAvailabledomain ]; then
+            echo -e $"This domain does not exist.\nPlease try another one"
+            exit;
+        else
+            ### Delete domain in /etc/hosts
+            newhost=${domain//./\\.}
+            sed -i "/$newhost/d" /etc/hosts
 
-			### Delete domain in /mnt/c/Windows/System32/drivers/etc/hosts (Windows Subsytem for Linux)
-			if [ -e /mnt/c/Windows/System32/drivers/etc/hosts ]
-			then
-				newhost=${domain//./\\.}
-				sed -i "/$newhost/d" /mnt/c/Windows/System32/drivers/etc/hosts
-			fi
+            ### Delete domain in /mnt/c/Windows/System32/drivers/etc/hosts (Windows Subsytem for Linux)
+            if [ -e /mnt/c/Windows/System32/drivers/etc/hosts ]
+            then
+                newhost=${domain//./\\.}
+                sed -i "/$newhost/d" /mnt/c/Windows/System32/drivers/etc/hosts
+            fi
 
-			### disable website
-			a2dissite $domain
+            ### disable website
+            a2dissite $domain
 
-			### restart Apache
-			/etc/init.d/apache2 reload
+            ### restart Apache
+            /etc/init.d/apache2 reload
 
-			### Delete virtual host rules files
-			rm $sitesAvailabledomain
-		fi
+            ### Delete virtual host rules files
+            rm $sitesAvailabledomain
+        fi
 
-		### check if directory exists or not
-		if [ -d $rootDir ]; then
-			echo -e $"Delete host root directory ? (y/n)"
-			read deldir
+        ### check if directory exists or not
+        if [ -d $rootDir ]; then
+            echo -e $"Delete host root directory ? (y/n)"
+            read deldir
 
-			if [ "$deldir" == 'y' -o "$deldir" == 'Y' ]; then
-				### Delete the directory
-				rm -rf $rootDir
-				echo -e $"Directory deleted"
-			else
-				echo -e $"Host directory conserved"
-			fi
-		else
-			echo -e $"Host directory not found. Ignored"
-		fi
+            if [ "$deldir" == 'y' -o "$deldir" == 'Y' ]; then
+                ### Delete the directory
+                rm -rf $rootDir
+                echo -e $"Directory deleted"
+            else
+                echo -e $"Host directory conserved"
+            fi
+        else
+            echo -e $"Host directory not found. Ignored"
+        fi
 
-		### show the finished message
-		echo -e $"Complete!\nYou just removed Virtual Host $domain"
-		exit 0;
+        ### show the finished message
+        echo -e $"Complete!\nYou just removed Virtual Host $domain"
+        exit 0;
 fi
