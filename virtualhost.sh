105946ca083b3a68c20742fc0c8fd831b552a558
Jacob Bearce <jacob@bearce.me>
2018-03-26 16:28:45 -0500

    Update virtualhost template

--- virtualhost.sh
+++ virtualhost.sh
@@ -68,24 +68,23 @@ if [ "$action" == 'create' ]
         fi
 
         ### create virtual host rules file
-        if ! echo "
-        <VirtualHost *:80>
-            ServerAdmin $email
-            ServerName $domain
-            ServerAlias $domain
-            DocumentRoot $rootDir
-            <Directory />
-                AllowOverride All
-            </Directory>
-            <Directory $rootDir>
-                Options Indexes FollowSymLinks MultiViews
-                AllowOverride all
-                Require all granted
-            </Directory>
-            ErrorLog /var/log/apache2/$domain-error.log
-            LogLevel error
-            CustomLog /var/log/apache2/$domain-access.log combined
-        </VirtualHost>" > $sitesAvailabledomain
+        if ! echo "<VirtualHost *:80>
+    ServerAdmin $email
+    ServerName $domain
+    ServerAlias $domain
+    DocumentRoot www.$rootDir
+    <Directory />
+        AllowOverride All
+    </Directory>
+    <Directory $rootDir>
+        Options FollowSymLinks MultiViews
+        AllowOverride all
+        Require all granted
+    </Directory>
+    ErrorLog /var/log/apache2/$domain-error.log
+    LogLevel error
+    CustomLog /var/log/apache2/$domain-access.log combined
+</VirtualHost>" > $sitesAvailabledomain
         then
             echo -e $"There is an ERROR creating $domain file"
             exit;
